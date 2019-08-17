a86a5f80fb31caa3ce34d890ee37b75b8bc9c54b
U-DESKTOP-HI8IHO4\Thomas <thomasrodgers271@gmail.com>
2019-08-17 16:22:14 -0700

    removed node_modules

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -1,2 +1,13 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection('')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'Thomas',
+    password: 'erwq',
+    database: 'top_songs_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    
+})
